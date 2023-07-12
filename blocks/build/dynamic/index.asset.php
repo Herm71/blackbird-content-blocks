2825902a8080e370a390255d2b966ee1b557cc32
Jason Chafin <jchafin@ucsc.edu>
2023-07-12 07:25:13 -0700

    update package-lock

--- blocks/build/dynamic/index.asset.php
+++ blocks/build/dynamic/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '2eb8616337812dbbfe1a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'cf6c965fdb0f84e92a61');
